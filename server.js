ab60f595a6165d7f158bcb81ab295fa72d9f50c9
Anishka Kesaria <kescotak@gmail.com>
2023-08-11 00:04:06 +0530

    Connected to DB

--- server.js
+++ server.js
@@ -1,7 +1,49 @@
 const express = require('express');
-const serverConfig = require('./config/server.config')
+const serverConfig = require('./config/server.config.js')
+const mongoose = require('mongoose');
+const dbConfig = require('./config/db.config.js')
+const userModel = require('./models/user.model.js')
 
 const app = express();
+
+//Logic to connect to DB
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection;
+
+db.on("error", ()=> {
+    console.log("Error while connecting to DB");
+});
+
+db.once("open", ()=> {
+    console.log("DB is connected");
+
+    init();
+});
+
+async function  init(){
+    //initialise mongo   
+    //create admin
+
+    //check if user is already present
+    let admin = userModel.findOne({
+        userId: "admin"
+    })
+
+    if(admin){
+        console.log("Admin is already present");
+        return;
+    }
+
+    admin = await userModel.create({
+        name: 'Vishwa Mohan',
+        userId: 'admin',
+        email: 'kankvish@gmail.com',
+        userType: 'ADMIN',
+        password: 'welcome1'
+    });
+    console.log(admin);
+}
+
 
 app.listen(serverConfig.PORT, ()=> {
     console.log(`Server started on port: ${serverConfig.PORT}`);
